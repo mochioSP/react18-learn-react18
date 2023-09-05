0e8210a23264c6246d9c727659e39d5727a6a0be
mochioSP <keeee50114@gmail.com>
2023-09-05 19:55:14 +0900

    transitionを理解するための簡易的なアプリの作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import "./App.css";
 
 import { AutoBatchEventHandler } from "./components/AutoBatchEventHandler";
 import { AutoBatchOther } from "./components/AutoBatchOther";
+import { Transition } from "./components/Transition";
 
 function App() {
   return (
     <div className="App">
       <AutoBatchEventHandler />
       <AutoBatchOther />
+      <hr />
+      <Transition />
     </div>
   );
 }
