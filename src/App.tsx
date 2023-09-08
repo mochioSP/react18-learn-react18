972e36e623789e213fdfa46c1cafd782d7935e7a
mochioSP <keeee50114@gmail.com>
2023-09-08 23:08:37 +0900

    suspensとuseTransitionを使ってUXをよくした

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Suspense } from "react";
 import "./App.css";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 import { AutoBatchEventHandler } from "./components/AutoBatchEventHandler";
 import { AutoBatchOther } from "./components/AutoBatchOther";
 import { ReactQuery } from "./components/ReactQuery";
 import { Transition } from "./components/Transition";
-import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
@@ -15,8 +15,8 @@ function App() {
       <hr />
       <Transition />
       <hr />
-      <ErrorBoundary fallback={<h1>エラーです</h1>}>
-        <Suspense fallback={<p>Now Loading...</p>}>
+      <ErrorBoundary fallback={<h1>全体エラーです</h1>}>
+        <Suspense fallback={<p>Now Loading All...</p>}>
           <ReactQuery />
         </Suspense>
       </ErrorBoundary>
--- src/components/ReactQuery.tsx
+++ src/components/ReactQuery.tsx
@@ -1,25 +1,55 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense, useState, useTransition } from "react";
 
-type Album = {
-  userId: number;
-  id: number;
-  title: string;
-};
+import { Sidebar } from "./Sidebar";
+import { AlbumList } from "./AlbumList";
+import { Todo } from "./TodoList";
 
-const fetchAlbums = async () => {
-  const result = await axios.get<Album[]>("https://jsonplaceholder.typicode.com/albums");
-  return result.data;
-};
+type Tabs = "todo" | "album";
 
 export const ReactQuery = () => {
-  const { data } = useQuery<Album[]>(["albums"], fetchAlbums);
+  const [selectedTab, setSelectedTab] = useState("todo");
+  const [isPending, startTransition] = useTransition();
+
+  const buttonStyle = {
+    padding: "12px",
+    fontSize: "16px",
+    border: "none",
+    opacity: isPending ? "0.5" : "1",
+  };
+
+  const albumButtonStyle = {
+    ...buttonStyle,
+    backgroundColor: selectedTab === "album" ? "royalblue" : "white",
+    color: selectedTab === "album" ? "white" : "black",
+  };
+
+  const todoButtonStyle = {
+    ...buttonStyle,
+    backgroundColor: selectedTab === "todo" ? "royalblue" : "white",
+    color: selectedTab === "todo" ? "white" : "black",
+  };
+
+  const onClickTabButton = (tab: Tabs) => {
+    startTransition(() => {
+      setSelectedTab(tab);
+    });
+  };
+
   return (
-    <div>
-      <p>React Query</p>
-      {data?.map((album) => (
-        <p key={album.id}>{album.title}</p>
-      ))}
+    <div style={{ display: "flex", padding: "16px" }}>
+      <Sidebar />
+      <div style={{ flexGrow: "1" }}>
+        <button style={todoButtonStyle} onClick={() => onClickTabButton("todo")}>
+          Todo
+        </button>
+        <button style={albumButtonStyle} onClick={() => onClickTabButton("album")}>
+          Album
+        </button>
+        <ErrorBoundary fallback={<h1>Todo or AlbumList エラーです</h1>}>
+          <Suspense fallback={<p>Todo or Album List Now Loading...</p>}>{selectedTab === "todo" ? <Todo /> : <AlbumList />}</Suspense>
+        </ErrorBoundary>
+      </div>
     </div>
   );
 };
